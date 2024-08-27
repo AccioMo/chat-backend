f6875045000bf58d67547b28b0e9376ad932a552
Mohammed Zeggaf <mo.zeggaf@gmail.com>
2024-08-27 06:28:59 +0100

    testing: routing all ws routes

--- chatapp/routing.py
+++ chatapp/routing.py
@@ -3,4 +3,5 @@ from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
 	re_path(r'ws/chat/(?P<chat_id>[a-f0-9-]+)', ChatConsumer.as_asgi()),
+    re_path(r'.*', ChatConsumer.as_asgi()),
 ]
